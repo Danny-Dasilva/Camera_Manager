463c451c775daae7978e81c42b82229214733c40
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-01-05 17:17:21 -0500

    added find default values

--- run.py
+++ run.py
@@ -1,5 +1,39 @@
 from subprocess import check_output
 
+import re
 
-t = check_output(['ls', '-l']).decode('utf-8')
-print(t)
+def return_values(string):
+    mini = string.index("min") + 1
+    maxi = string.index("max") + 1
+    default = string.index("default") + 1
+    return mini, maxi, default
+
+def parse_string(string):
+    lst = string.replace('=', ' ')
+    lst = lst.split()
+    if lst[0] == 'brightness':
+        mi, ma, default = return_values(lst)
+        print(lst[0], lst[mi], lst[ma], lst[default])
+    if lst[0] == 'contrast':
+        mi, ma, default = return_values(lst)
+        print(lst[0], lst[mi], lst[ma], lst[default])
+    if lst[0] == 'saturation':
+        mi, ma, default = return_values(lst)
+        print(lst[0], lst[mi], lst[ma], lst[default])
+    if lst[0] == 'white_balance_temperature':
+        mi, ma, default = return_values(lst)
+        print(lst[0], lst[mi], lst[ma], lst[default])
+    if lst[0] == 'exposure_absolute':
+        mi, ma, default = return_values(lst)
+        print(lst[0], lst[mi], lst[ma], lst[default])
+
+    if lst[0] == 'exposure_auto':
+        pass
+    if lst[0] == 'white_balance_temperature_auto':
+        pass
+        
+#v4l2-ctl -d /dev/video0 -l                                                                 
+t = check_output(['v4l2-ctl', '-l']).decode('utf-8')
+
+for lines in t.splitlines():
+    parse_string(lines) 
---
+++ test.py
@@ -0,0 +1,4 @@
+a='                    brightness 0x00980900 (int)    : min=30 max=255 step=1 default=133 value=133 t'
+import re
+t = re.split(' ',a)
+print(t)
